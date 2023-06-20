374a4e6a1f78f1993d3c9858c8ee5ab09cf81896
Mehmet Ali Battal <mehmetalibattal@gmail.com>
2023-06-20 22:46:19 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    //Context sınıfları DbContext sınıfından miras alır
+    public class Context: DbContext
+    {
+        //OnConfiguring metodunun içinde bağlantı stringimizi tanımlıyoruz
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = DESKTOP-Q3DTBEF; database = CoreBlogDb; integrated security = true;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //ErişimBelirleyiciTürü DeğişkenTürü İsim {get set}
+        [Key]
+        public int CategoryID { get; set; }
+        public string CtategoryNanem { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
